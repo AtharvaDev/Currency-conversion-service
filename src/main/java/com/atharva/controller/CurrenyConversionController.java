b4442f71432c2e3b8cb3de9ebefa2ec0b9177746
Atharva Deshmukh <atharva1525@gmail.com>
2021-11-18 10:38:25 +0530

    init

--- src/main/java/com/atharva/controller/CurrenyConversionController.java
+++ src/main/java/com/atharva/controller/CurrenyConversionController.java
@@ -18,16 +18,13 @@ public class CurrenyConversionController {
 	public CurrencyConversion calculateCurrencyConversion(
 			@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity
 			) {
-		System.out.println("1" + from + to + quantity);
 
 		HashMap<String, String> uriVaribales = new HashMap<>();
 		uriVaribales.put("from", from);
 		uriVaribales.put("to", to);
 		
-		System.out.println("2" + uriVaribales);
 		ResponseEntity<CurrencyConversion> responseEntity = new RestTemplate().getForEntity("http://localhost:8000/currency-exchange/from/{from}/to/{to}", CurrencyConversion.class, uriVaribales);
 		
-		System.out.println("3");
 		CurrencyConversion currencyConversion = responseEntity.getBody();
 		
 		return new CurrencyConversion(currencyConversion.getId(), from, to, quantity, currencyConversion.getConversionMultiple(), quantity.multiply(currencyConversion.getConversionMultiple()), currencyConversion.getEnvironment() );
