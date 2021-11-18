6de8f0d6404bce1df8ca62867acfaa7652dd179a
Atharva Deshmukh <atharva1525@gmail.com>
2021-11-18 13:04:47 +0530

    fiegn api added

--- src/main/java/com/atharva/controller/CurrenyConversionController.java
+++ src/main/java/com/atharva/controller/CurrenyConversionController.java
@@ -3,6 +3,7 @@ package com.atharva.controller;
 import java.math.BigDecimal;
 import java.util.HashMap;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,9 +11,16 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import com.atharva.dto.CurrencyConversion;
+import com.atharva.proxy.CurrencyExchangeProxy;
 
 @RestController
 public class CurrenyConversionController {
+	
+	@Autowired
+	private CurrencyExchangeProxy proxy;
+
+//	http://localhost:8100/currency-conversion/from/CAD/to/INR/quantity/10
+//	http://localhost:8100/currency-conversion/from/USD/to/INR/quantity/10
 
 	@GetMapping("currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
 	public CurrencyConversion calculateCurrencyConversion(
@@ -27,7 +35,19 @@ public class CurrenyConversionController {
 		
 		CurrencyConversion currencyConversion = responseEntity.getBody();
 		
-		return new CurrencyConversion(currencyConversion.getId(), from, to, quantity, currencyConversion.getConversionMultiple(), quantity.multiply(currencyConversion.getConversionMultiple()), currencyConversion.getEnvironment() );
-		
+		return new CurrencyConversion(currencyConversion.getId(), from, to, quantity, currencyConversion.getConversionMultiple(), quantity.multiply(currencyConversion.getConversionMultiple()), currencyConversion.getEnvironment());	
 	}
+	
+//	http://localhost:8100/currency-conversion-feign/from/CAD/to/INR/quantity/10
+//	http://localhost:8100/currency-conversion-feign/from/USD/to/INR/quantity/10
+
+	@GetMapping("currency-conversion-feign/from/{from}/to/{to}/quantity/{quantity}")
+	public CurrencyConversion calculateCurrencyConversionFeign(
+			@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity) {
+
+		CurrencyConversion currencyConversion = proxy.retriveExchangeValue(from, to);
+		
+		return new CurrencyConversion(currencyConversion.getId(), from, to, quantity, currencyConversion.getConversionMultiple(), quantity.multiply(currencyConversion.getConversionMultiple()), currencyConversion.getEnvironment()+ " feign");	
+	}
+
 }
